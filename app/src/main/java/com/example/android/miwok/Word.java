167c9769134b42c76b9b4560e825f55e22f5115c
RayanAlkhelaiwi <r.n.m.k@live.com>
2017-08-29 21:52:19 +0300

    Added capability to play sound to each vocabulary

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -9,19 +9,22 @@ public class Word {
     private final int NO_IMAGE = -1;
     private String mDefaultTranslation = "";
     private String mMiwokTranslation = "";
+    private int mListAudio = 0;
     private int mListImage = 0;
 
-    public Word(String defaultTranslation, String miwokTranslation) {
+    public Word(String defaultTranslation, String miwokTranslation, int listAudio) {
 
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mListAudio = listAudio;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int listImage) {
+    public Word(String defaultTranslation, String miwokTranslation, int listImage, int listAudio) {
 
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mListImage = listImage;
+        mListAudio = listAudio;
     }
 
     public String getmDefaultTranslation() {
@@ -35,6 +38,10 @@ public class Word {
 
     public int getListImage() {
         return mListImage;
+    }
+
+    public int getListAudio() {
+        return mListAudio;
     }
 
     public boolean hasImage() {
